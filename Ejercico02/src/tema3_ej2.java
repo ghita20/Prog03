a1e46934493da12a7fa3c3f1c42bd7754042d321
Gheorghe Pop <ghita.europa5b@gmail.com>
2016-11-04 18:04:40 +0100

    Ejercicio 2 completado.

---
+++ Ejercico02/src/tema3_ej2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class tema3_ej2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
+		int N;
+		do{
+			System.out.print("Introduce el valor de N:");
+			N=teclado.nextInt();
+		}while(N==1 || N==100);
+		int i=1;
+		while(i<=10)
+			System.out.println(N +" x " +i +" = " +N*i++);
+		
+	}
+
+}
