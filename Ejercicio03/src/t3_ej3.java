0c7c03e244f0016bdeee110f2ae384961202ad03
eeralexito <alexrosca180596@gmail.com>
2016-11-04 19:45:08 +0100

    Ejercicio 7 completado.

---
+++ Ejercicio03/src/t3_ej3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class t3_ej3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
+		int n,i=1;
+		System.out.println("Introduce el numero");
+		n=teclado.nextInt();
+		while(n>i){
+			if(n%i==0)
+			System.out.println(i);	
+			i++;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
