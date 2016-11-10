3039f9dc564277e6d29383eae0f5b8d7991acac0
Gheorghe Pop <ghita.europa5b@gmail.com>
2016-11-10 19:24:45 +0100

    Ejercicio 15 realizado.

---
+++ Ejercicio15/src/tema3_ej15.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class tema3_ej15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
+		int a,b,c,mayor=0,medio=0,menor=0;
+		System.out.println("Introduce a: ");
+		a=teclado.nextInt();
+		System.out.println("Introduce b: ");
+		b=teclado.nextInt();
+		System.out.println("Introduce c: ");
+		c=teclado.nextInt();
+		
+		if(a>b)
+			if(a>c){
+				mayor=a;
+				if(b>c){
+					medio=b;
+					menor=c;
+				} else{
+					medio=c;
+					menor=b;
+				}
+				
+			} else{
+				mayor=c;
+				medio=a;
+				menor=b;
+			}
+		else
+			if(b>c)
+				if(c>a){
+					menor=a;
+					medio=c;
+					mayor=b;
+				} else{
+					menor=c;
+					medio=a;
+					mayor=b;
+				}
+			else{
+				mayor=c;
+				medio=b;
+				menor=a;
+			}
+		System.out.println(+menor +" " +medio +" " +mayor);
+	}
+
+}
