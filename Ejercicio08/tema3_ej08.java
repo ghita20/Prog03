46a9137365a1bfe25a4e2608f8d8f5bb0a132c35
Gheorghe Pop <Bideos@192.168.1.34>
2016-11-11 23:22:12 +0100

    Ejercicio 8 completo.

--- Ejercicio08/tema3_ej08.java
+++ Ejercicio08/tema3_ej08.java
@@ -1,24 +1,20 @@
+//este programa calcula el factorial de un número
 import java.util.Scanner;
-
 public class tema3_ej8 {
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
-	int n,i=2,f=1;
-	System.out.println("Introduce el numero que deseas realizar el factorial: ");
-	n=teclado.nextInt();
-
-	if(n<=0)
-		System.out.println("El numero es incorrecto");
-	if(n==1)
-		System.out.println("El factorial es: 1");
-	else do{
+		int n,i=2,f=1;
+		System.out.println("Introduce el numero que deseas realizar el factorial: ");
+		n=teclado.nextInt();
 	
-		f*=i;
-		i++;
-	}while(n>=i);
+		if(n==1);//si n==1 ya no calculo el factorial y paso ya al System.out.print
+		else do{
+			f*=i;
+			i++;
+		}while(n>=i);
 		
-	if(n!=1 && n!=0)
-		System.out.println("El factorial es: " +f);
+		//si n>0 imprimo su factorial si no imprimo que es incorrecto
+		System.out.println(n>0?"El factorial es: " +f:"El número es incorrecto");
 	}
 
 }
