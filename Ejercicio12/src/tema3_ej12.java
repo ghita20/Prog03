0b15fa0346369ec2151934c38e62a54c62349b44
Gheorghe Pop <Bideos@192.168.1.34>
2016-11-11 22:51:39 +0100

    Ejercicio 12 acabado.

--- Ejercicio12/src/tema3_ej12.java
+++ Ejercicio12/src/tema3_ej12.java
@@ -1,5 +1,6 @@
+//Programa con un menú con 3 opciones y
+//según la opción pedirá ciertos datos
 import java.util.Scanner;
-
 public class tema3_ej12 {
 
 	public static void main(String[] args) {
@@ -10,7 +11,7 @@ public class tema3_ej12 {
 			System.out.println("Elige un tipo de figura.");
 			System.out.println("1.Triángulo 2.Cuadrado 3.Rectángulo");
 			opcion=teclado.nextInt();
-		}while(opcion<1 || opcion>3);
+		}while(opcion<1 || opcion>3);//si el número introducido es mayor que 3 y menor que 1 vuelvo a pedirlo
 		
 		switch(opcion){
 			case 1: System.out.println("Introduce la base: ");
@@ -29,7 +30,7 @@ public class tema3_ej12 {
 					altura=teclado.nextInt();
 					System.out.println("El área del rectángulo es: " +base*altura); break;
 					
-		}
+		}//fin del switch
 	}
 
 }
