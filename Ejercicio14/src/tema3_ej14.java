7c50a0ac98ce0b4e35047464b39a614a88a73e5a
Gheorghe Pop <Bideos@192.168.1.34>
2016-11-11 22:43:53 +0100

    Ejercicio 14 acabado.

--- Ejercicio14/src/tema3_ej14.java
+++ Ejercicio14/src/tema3_ej14.java
@@ -1,5 +1,5 @@
+//Lee un número de 3 cifras y lo invierte
 import java.util.Scanner;
-
 public class tema3_ej14 {
 
 	public static void main(String[] args) {
@@ -10,7 +10,8 @@ public class tema3_ej14 {
 		do{
 		System.out.println("Introduce un número de 3 cifras.");
 		N=teclado.nextInt();
-		r=N;
+		r=N;//esta variable la uso para hacer las operaciones con N para no modificar el contenido de N
+		//en este bucle averiguo la longitud de N
 		while(!cosa){
 			if(r%10!=0)
 				longitud++;
@@ -19,12 +20,12 @@ public class tema3_ej14 {
 			r/=10;
 		}
 
-		}while(longitud!=3);
+		}while(longitud!=3);//si la longitud es diferente de 3 pido N de nuevo
 		int i=1;
 		r=N;
 		while(i<=3){
-			System.out.print(r%10);
-			r/=10;
+			System.out.print(r%10);//imprimo el ultimo número ya que por ejemplo 123%10=3
+			r/=10;//voy quitando un número ya que por ejemplo 123/10=12
 			i++;
 			
 		}
