4c87a547c8330e47720dff1200835381ac88e8ab
ghita20 <ghita.europa5b@gmail.com>
2016-11-11 23:32:38 +0100

    Update tema3_ej7bueno.java

--- Ejercicio07bueno/src/tema3_ej7bueno.java
+++ Ejercicio07bueno/src/tema3_ej7bueno.java
@@ -1,5 +1,6 @@
+//Programa que pide numeros hasta que introduzcas 0 y luego
+// imprime la media de todos ellos, el mayor y el menor.
 import java.util.Scanner;
-
 public class tema3_ej7bueno {
 
 	public static void main(String[] args) {
@@ -9,15 +10,18 @@ public class tema3_ej7bueno {
 		do{
 			System.out.println("Introduce valores: ");
 			n=teclado.nextInt();
-			if(n!=0){
+			if(n!=0){ //esta condición es por si introducimos 0 ya no realice las comprobaciones (estropearía la media y el número menor)
 				cantidad++;
 				suma+=n;
-			if(n>elMayor)
-				elMayor=n;
-			if(cantidad==1)
+			//si N es mayor que elMayor actualizamos el número Mayor
+			elMayor=n>elMayor?n:elMayor;	
+			
+			if(cantidad==1)//el primer número srá el mas pequeño
 				elMenor=n;
-			else if(n<elMenor)
-				elMenor=n;}
+			else 
+			//luego vamos comprobando si hay alguno mas pequeño que el primero
+			elMenor=n<elMenor?n:elMenor;				
+			}
 		}while(n!=0);
 		
 		System.out.println("La media es: " +suma/cantidad);
