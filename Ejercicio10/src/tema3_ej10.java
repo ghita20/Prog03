eba8e6e3efdb35afe2347c305b289ecb73695512
Gheorghe Pop <Bideos@192.168.1.34>
2016-11-11 23:03:58 +0100

    Ejercicio 10 realizado con éxito!!

--- Ejercicio10/src/tema3_ej10.java
+++ Ejercicio10/src/tema3_ej10.java
@@ -1,5 +1,6 @@
+//Dado un numero N leído por teclado encontrar el numero primo mas cercano a él por exceso.
+ 
 import java.util.Scanner;
-
 public class tema3_ej10 {
 
 	public static void main(String[] args) {
@@ -10,20 +11,22 @@ public class tema3_ej10 {
 
 		System.out.println("Introduce N: ");
 		N=teclado.nextInt();
+		//i será N+1 para no probar si N es primo a que queremos
+		//averiguar el siguiente número primo después de N, no nos importa si N es primo
 		i=N+1;
 
 		while(!cosa){
-			primo=0;
+			primo=0;//reinicio variables
 			k=1;
 			while(k<=i){
-				if(i%k==0)
-					primo++;
+				//si k es divisor de i aumento primo en 1
+				primo=i%k==0?++primo:primo;
 				k++;
 			}
 			
 			if(primo==2)
-				cosa=true;
-			else
+				cosa=true;//si encuentro un número que solo tiene 2 divisores(primo) 
+			else //si no, sigo probando
 				i++;
 		}
 		
