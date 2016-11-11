911c1b694bef93e415874a73801e012064d6c6d8
Gheorghe Pop <Bideos@192.168.1.34>
2016-11-11 22:29:22 +0100

    Ejercicio 23 acabado.

--- Ejercicio23/src/tema3_ej23.java
+++ Ejercicio23/src/tema3_ej23.java
@@ -5,30 +5,24 @@ public class tema3_ej23 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner(System.in);
-		int numero=0,ceros=0;
-		boolean cosa=true;
+		int n, residuo=0,cero=0;
 		do{
-			System.out.println("Introduce el número: ");
-			numero=teclado.nextInt();
-			int longitud=0,residuo=0;
-			while(residuo!=0){
-				if(longitud==0)
-					residuo=numero;
-				residuo/=10;
-				if(residuo!=0)
-					longitud++;
-			}
-			residuo=numero;
-			while(cosa){
-				residuo/=10;
-				if(residuo==0)
-					cosa=false;
-				else
-					ceros++;
-			}
+			System.out.println("introduce un valor: ");
+			n=teclado.nextInt();
 			
-		}while(numero>=0);
-		System.out.println(ceros);
+			if(n==0) //si n=0 aumento 0 en 1
+				cero++;
+			else{
+				residuo=n; //esta variable la uso para poder ir dividiendo entre 10
+				while(residuo!=0){
+				cero=residuo%10==0?cero+1:cero; //si encuentra un 0 aumenta en 1 cero
+					residuo/=10;
+				}//fin del while
+			}//fin del else
+				
+			
+		}while(n>=0);
+		System.out.println("La cantidad de 0s introducidos es: " +cero);
 	}
 
 }
