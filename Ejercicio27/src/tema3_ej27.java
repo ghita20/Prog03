a3c5ea13029119ccadb1454e83d2da2334336c42
Gheorghe Pop <Bideos@192.168.1.33>
2016-11-18 23:55:18 +0100

    Ejercicio 27.

--- Ejercicio27/src/tema3_ej27.java
+++ Ejercicio27/src/tema3_ej27.java
@@ -16,12 +16,12 @@ public class tema3_ej27 {
 			System.out.println("Introduce el numero 2: ");
 			n2=teclado.nextInt();
 			}while(n2<=n1);
-		
+	
 		do{
 			System.out.println("Elige una opción: ");
 			System.out.println("Imprimir el resultado - 1.Incluyendo ambos números 2.Sin incluirlos");
 			opcion=teclado.nextInt();
-		}while(opcion<1 && opcion>2);
+		}while(opcion>2 || opcion<1);
 		
 		
 		switch(opcion){
@@ -42,11 +42,6 @@ public class tema3_ej27 {
 					sumaPares+=i;
 			}
 			
-			System.out.println("Los numeros primos comprendidos entre " +n1 +" y " +n2 + " son: ");
-			System.out.println(primosFinales);
-			System.out.println("La suma de los numeros pares es: " +sumaPares);
-			System.out.println("La suma de los impares es: " +sumaImpares); 
-		
 			break;
 		case 2:
 			
@@ -65,13 +60,14 @@ public class tema3_ej27 {
 					else
 						sumaPares+=i;
 				}
-				
-				System.out.println("Los numeros primos comprendidos entre " +n1 +" y " +n2 + " son: ");
-				System.out.println(primosFinales);
-				System.out.println("La suma de los numeros pares es: " +sumaPares);
-				System.out.println("La suma de los impares es: " +sumaImpares); 
-			break;
+				break;
 		}
+		
+		System.out.println("Los numeros primos comprendidos entre " +n1 +" y " +n2 + " son: ");
+		System.out.println(primosFinales);
+		System.out.println("La suma de los numeros pares es: " +sumaPares);
+		System.out.println("La suma de los impares es: " +sumaImpares); 
+	
 			
 	}
 
