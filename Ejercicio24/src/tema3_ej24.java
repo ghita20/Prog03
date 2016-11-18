06e71598b415620e2fe6049f507325f2c96895e5
Gheorghe Pop <Bideos@192.168.1.33>
2016-11-18 23:02:34 +0100

    Ejercicio 24 completado

--- Ejercicio24/src/tema3_ej24.java
+++ Ejercicio24/src/tema3_ej24.java
@@ -1,9 +1,25 @@
-
+//Programa que obtenga el producto de dos números enteros positivos dados por teclado mediante
+//sumas sucesivas y lo muestre en pantalla.
+import java.util.Scanner;
 public class tema3_ej24 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner teclado=new Scanner(System.in);
+		int n1,n2,suma=0,i=1;
+		
+		System.out.println("Introduce el numero 1: ");
+		n1=teclado.nextInt();
+		
+		System.out.println("Introduce el numero 2: ");
+		n2=teclado.nextInt();
+		
+		while(i<=n2){
+			suma+=n1;
+			i++;
+		}
+		System.out.println("El producto de " +n1 +" x " +n2 +" = " +suma);
+		
 	}
 
 }
