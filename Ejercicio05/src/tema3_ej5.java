c9da50c0758a4f54ac88c7fb2b1ecc609edd2373
Gheorghe Pop <Bideos@192.168.1.33>
2016-11-19 16:21:58 +0100

    Ejercicio 05.

--- Ejercicio05/src/tema3_ej5.java
+++ Ejercicio05/src/tema3_ej5.java
@@ -1,5 +1,6 @@
+//Programa que lee números hasta que el número introducido sea 0
+//seguidamente imprime la suma y cantidad de numeros introducidos
 import java.util.Scanner;
-
 public class tema3_ej5 {
 
 	public static void main(String[] args) {
@@ -9,11 +10,16 @@ public class tema3_ej5 {
 		do{
 			System.out.println("Introduce el numero: ");
 			n=teclado.nextInt();
+			//si introducimos 0 no queremos que nos aumente el contador 
+			//asi que la solución es un if
 			if(n!=0){
 				contador++;
-				suma+=n;}
-			}while(n!=0); 		
-			System.out.println("La suma es: "+ suma +" "+"La cantidad de valores introducidos es: "+contador);
+				suma+=n;
+				}//fin del if
+			
+			}while(n!=0); //hace el do mientras n sea diferente de 0
+		
+		System.out.println("La suma es: "+ suma +" "+". La cantidad de valores introducidos es: "+contador);
 			
 			
 			
