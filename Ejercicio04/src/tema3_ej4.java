a86758d244d55ca6c93a43cf9ac5b070dce268c8
Gheorghe Pop <Bideos@192.168.1.33>
2016-11-19 16:16:29 +0100

    Ejercicio 04 corregido.

--- Ejercicio04/src/tema3_ej4.java
+++ Ejercicio04/src/tema3_ej4.java
@@ -1,22 +1,32 @@
+//Programa que dado un numero n imrpime si es o no primo
 import java.util.Scanner;
 public class tema3_ej4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner(System.in);
-		int n,v=1,p=0;
-		System.out.println("Introduce el valor");
+		int n,i=2;
+		boolean esPrimo=true;
+		
+		System.out.println("Introduce el número: ");
 		n=teclado.nextInt();
-		while(n>=v){
-		if(n%v==0)
-		p++;
-		v++;}
-		if(p==2)
-			System.out.println("Es primo");
-		else 
-			System.out.println("No es primo");
 		
+		while(i<n && esPrimo){
+			//si encuentra algún divisor significará que no es primo y salimos del bucle
+			esPrimo= n%i==0?false:esPrimo;
+			
+			i++;
+		}//fin del while
 		
+		//0 y 1 no son primos
+		if(n<=0 || n==1)
+			System.out.println("No es primo.");
+		else
+			//si esPrimo=true imprimo que es primo, si no, lo contrario
+			System.out.println(esPrimo?"Es primo.":"No es primo.");
+		
+
+
 	}
 
 }
