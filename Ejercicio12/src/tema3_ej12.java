cf1ee23d7a44082625d8347e74a2af922016267e
ghita20 <ghita.europa5b@gmail.com>
2016-11-22 22:39:26 +0100

    Update tema3_ej12.java

--- Ejercicio12/src/tema3_ej12.java
+++ Ejercicio12/src/tema3_ej12.java
@@ -1,5 +1,5 @@
-//Programa con un men˙ con 3 opciones y
-//seg˙n la opciÛn pedir· ciertos datos
+//Programa con un men√∫ con 3 opciones y
+//seg√∫n la opci√≥n pedir√° ciertos datos
 import java.util.Scanner;
 public class tema3_ej12 {
 
@@ -9,26 +9,23 @@ public class tema3_ej12 {
 		int opcion,base=0,altura=0;
 		do{
 			System.out.println("Elige un tipo de figura.");
-			System.out.println("1.Tri·ngulo 2.Cuadrado 3.Rect·ngulo");
+			System.out.println("1.Tri√°ngulo 2.Cuadrado 3.Rect√°ngulo");
 			opcion=teclado.nextInt();
-		}while(opcion<1 || opcion>3);//si el n˙mero introducido es mayor que 3 y menor que 1 vuelvo a pedirlo
+		}while(opcion<1 || opcion>3);//si el n√∫mero introducido es mayor que 3 y menor que 1 vuelvo a pedirlo
+		
+		 System.out.println("Introduce la base: ");
+		 base=teclado.nextInt();
 		
 		switch(opcion){
-			case 1: System.out.println("Introduce la base: ");
-					base=teclado.nextInt();
-					System.out.println("Introduce la altura: ");
+			case 1:		System.out.println("Introduce la altura: ");
 					altura=teclado.nextInt();
-					System.out.println("El ·rea del triangulo es: " +base*altura/2); break;
+					System.out.println("El √°rea del triangulo es: " +base*altura/2); break;
 			case 2: 
-					System.out.println("Introduce la base: ");
-					base=teclado.nextInt();
-					System.out.println("El ·rea del cuadrado es: " +Math.pow(base, 2)); break;
+					System.out.println("El √°rea del cuadrado es: " +Math.pow(base, 2)); break;
 			case 3: 
-					System.out.println("Introduce la base: ");
-					base=teclado.nextInt();
 					System.out.println("Introduce la altura: ");
 					altura=teclado.nextInt();
-					System.out.println("El ·rea del rect·ngulo es: " +base*altura); break;
+					System.out.println("El √°rea del rect√°ngulo es: " +base*altura); break;
 					
 		}//fin del switch
 	}
